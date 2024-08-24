5544af86583b217ed5826dc4798b68d4537b6a7c
himanishpuri <himanishpuri2203@gmail.com>
2024-08-24 22:42:36 +0530

    deleted branch

--- src/components/teamPageComponents/Card/Card Imgs/CardimgIndex.js
+++
@@ -1,9 +0,0 @@
-import Linkdinlogo from "../Card Imgs/Linkdinlogo.svg"
-import Googlelogo from "../Card Imgs/Googlelogo.svg"
-import Instagramlogo from "../Card Imgs/Instagramlogo.svg"
-import person from "../Card Imgs/person1.svg"
-
-export const linkdin = Linkdinlogo;
-export const google = Googlelogo;
-export const instagram = Instagramlogo;
-export const p1 = person;
