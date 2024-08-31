6fc72c55225454f84a862ec2971d9d959e9a7d77
Pranav Vaish <pranavvaish20@gmail.com>
2024-08-31 22:32:16 +0530

    a

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -2,24 +2,24 @@ import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 export const Navbar = () => {
-  const [showOptions, setShowOptions] = useState(false);
+	const [showOptions, setShowOptions] = useState(false);
 
-  const toggleMenu = () => {
-    setShowOptions(!showOptions);
-  };
+	const toggleMenu = () => {
+		setShowOptions(!showOptions);
+	};
 
-  return (
-    <div className={styles.NavPage}>
-      <div className={styles.Header}>
+	return (
+		<div className={styles.NavPage}>
+			<div className={styles.Header}>
 				<div className={styles.logo}>
 					<img
-						src="./src/assets/images/logo.png"
+						src="./src/assets/images/ISTE Thapar Chapter Logo blue.png"
 						alt="logo"
 					/>
 				</div>
-        <div className={styles.hamburger} onClick={toggleMenu}>
-          &#9776;
-        </div>
+				<div className={styles.hamburger} onClick={toggleMenu}>
+					&#9776;
+				</div>
 				<div className={`${styles.Options} ${showOptions ? styles.show : ""}`}>
 					<h1>
 						<Link to="/">Home</Link>
@@ -30,9 +30,9 @@ export const Navbar = () => {
 					<h1>
 						<Link to="/team">Team</Link>
 					</h1>
-				<Link to="/contact">Contact
-					{/* <button className={styles.contact}></button> */}
-				</Link>
+					<Link to="/contact">Contact
+						{/* <button className={styles.contact}></button> */}
+					</Link>
 				</div>
 			</div>
 		</div>
