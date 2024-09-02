e278a140f18d8ee02ed7f7ceb0c67bd7887b45fa
AabharanGupta <aabharangupta1001@gmail.com>
2024-09-02 11:49:51 +0530

    Revert ""Updated Navbar component with changes to logo, hamburger menu, and contact link""
    This reverts commit 0536780cce2ae6b9f876483ecb3135d1f06d312d, reversing
    changes made to be9117bfeb27612c0ca2838b28d4ef7cdda85e47.

--- src/components/event/Event.jsx
+++ src/components/event/Event.jsx
@@ -3,29 +3,37 @@ import "./Event.css";
 import EventList from "./EventList";
 import EventDetail from "./EventDetail";
 import { sample } from "../../assets/images/index.js";
-
+import { sample1 } from "../../assets/images/index.js";
+import { sample2 } from "../../assets/images/index.js";
+import { sample3 } from "../../assets/images/index.js";
+import { sample4 } from "../../assets/images/index.js";
+import { sample5 } from "../../assets/images/index.js";
+import { sample6 } from "../../assets/images/index.js";
+import { sample7 } from "../../assets/images/index.js";
+import { sample8 } from "../../assets/images/index.js";
+import { sample9 } from "../../assets/images/index.js";
+import { sample10 } from "../../assets/images/index.js";
+import { sample11 } from "../../assets/images/index.js";
 const events = [
 	{
-		name: "Orientation",
+		name: "Orientation (2023)",
 		intro: "Welcome to the ISTE community! During the orientation, new members were introduced to our dynamic organization, delved into exciting projects, and embarked on their tech journeys. They joined us to learn, connect, and innovate, forming valuable connections along the way. Together, we anticipated advancing technology and shaping the future through everyone s unique contributions.",
 		subEvents: [
 			{
-				ev1: "Sub Event 1.1",
-				detail1: "Short heading 1.1",
-				detail2: "Loreum 1.1",
+				ev1: "Orientation",
+				// detail1: "Short heading 1.1",
+				// detail2: "Loreum 1.1",
 				img1: sample,
 			},
 			{
-				ev1: "Sub Event 1.2",
-				detail1: "Short heading 1.1",
-				detail2: "Loreum 1.1",
-				img1: sample,
+				// detail1: "Short heading 1.1",
+				// detail2: "Loreum 1.1",
+				img1: sample1,
 			},
 			{
-				ev1: "Sub Event 1.3",
-				detail1: "Short heading 1.1",
-				detail2: "Loreum 1.1",
-				img1: sample,
+				// detail1: "Short heading 1.1",
+				// detail2: "Loreum 1.1",
+				img1: sample2,
 			},
 		],
 	},
@@ -34,22 +42,21 @@ const events = [
 		intro: "Experience the thrill of Xpedition, a distinguished treasure hunt event hosted by ISTE. From the challenge of unraveling cryptic clues to the suspense of navigating intricate bomb puzzles, this event was filled with unforgettable experiences. XPEDITION combined adventure, wit, and excitement, creating adrenaline-filled moments that participants will never forget.",
 		subEvents: [
 			{
-				ev1: "Sub Event 1.1",
-				detail1: "Short heading 1.1",
-				detail2: "Loreum 1.1",
-				img1: sample,
+				ev1: "Xpedition",
+				// detail1: "Short heading 1.1",
+				// detail2: "Loreum 1.1",
+				img1: sample3,
 			},
 			{
-				ev1: "Sub Event 1.2",
-				detail1: "Short heading 1.1",
-				detail2: "Loreum 1.1",
-				img1: sample,
+				// detail1: "Short heading 1.1",
+				// detail2: "Loreum 1.1",
+				img1: sample4,
 			},
 			{
-				ev1: "Sub Event 1.3",
-				detail1: "Short heading 1.1",
-				detail2: "Loreum 1.1",
-				img1: sample,
+
+				// detail1: "Short heading 1.1",
+				// detail2: "Loreum 1.1",
+				img1: sample5,
 			},
 		],
 	},
@@ -58,22 +65,21 @@ const events = [
 		intro: "Relive the excitement of Omega, the ultimate competitive programming event hosted by ISTE! This electrifying competition spanned two intense rounds, where participants demonstrated their exceptional coding skills, creativity, and problem-solving abilities. Omega was a thrilling journey of innovation and excellence, highlighting the remarkable talents of our community.",
 		subEvents: [
 			{
-				ev1: "Sub Event 1.1",
-				detail1: "Short heading 1.1",
-				detail2: "Loreum 1.1",
-				img1: sample,
+				ev1: "Omega",
+				// detail1: "Short heading 1.1",
+				// detail2: "Loreum 1.1",
+				img1: sample6,
 			},
 			{
-				ev1: "Sub Event 1.2",
-				detail1: "Short heading 1.1",
-				detail2: "Loreum 1.1",
-				img1: sample,
+				// detail1: "Short heading 1.1",
+				// detail2: "Loreum 1.1",
+				img1: sample7,
 			},
 			{
-				ev1: "Sub Event 1.3",
-				detail1: "Short heading 1.1",
-				detail2: "Loreum 1.1",
-				img1: sample,
+
+				// detail1: "Short heading 1.1",
+				// detail2: "Loreum 1.1",
+				img1: sample8,
 			},
 		],
 	},
@@ -82,22 +88,20 @@ const events = [
 		intro: "Colloquium, the flagship event of our technical society, was a much-awaited and an excitement-loaded four-day celebration of innovation and intellect.It featured unique sub-events that tested participants skills in coding without sight, creative electronic project building, strategic problem-solving in a competitive setting, and quick-witted knowledge battles.",
 		subEvents: [
 			{
-				ev1: "Sub Event 1.1",
-				detail1: "Short heading 1.1",
-				detail2: "Loreum 1.1",
-				img1: sample,
+				ev1: "Colloquium",
+				// detail1: "Short heading 1.1",
+				// detail2: "Loreum 1.1",
+				img1: sample9,
 			},
 			{
-				ev1: "Sub Event 1.2",
-				detail1: "Short heading 1.1",
-				detail2: "Loreum 1.1",
-				img1: sample,
+				// detail1: "Short heading 1.1",
+				// detail2: "Loreum 1.1",
+				img1: sample10,
 			},
 			{
-				ev1: "Sub Event 1.3",
-				detail1: "Short heading 1.1",
-				detail2: "Loreum 1.1",
-				img1: sample,
+				// detail1: "Short heading 1.1",
+				// detail2: "Loreum 1.1",
+				img1: sample11,
 			},
 		],
 	},
@@ -112,9 +116,7 @@ const Event = () => {
 	return (
 		<div className="app-container">
 			<div
-				className={`event-list-container ${
-					selectedEvent ? "hide-on-small-screen" : ""
-				}`}
+				className={`event-list-container `}
 			>
 				<EventList
 					events={events}
@@ -122,9 +124,7 @@ const Event = () => {
 				/>
 			</div>
 			<div
-				className={`event-detail-container ${
-					selectedEvent ? "" : "hide-on-small-screen"
-				}`}
+				className={`event-detail-container `}
 			>
 				<EventDetail event={selectedEvent} />
 			</div>
---
+++ src/components/teamPageComponents/TeamPageNavbar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+export const TeamPageNavbar = () => {
+	const [showOptions, setShowOptions] = useState(false);
+
+	const toggleMenu = () => {
+		setShowOptions(!showOptions);
+	};
+
+	return (
+		<div className={styles.NavPage}>
+			<div className={styles.Header}>
+				<div className={styles.logo}>
+					<img
+						src="./src/assets/images/logo.png"
+						alt="logo"
+					/>
+				</div>
+				<div
+					className={styles.hamburger}
+					onClick={toggleMenu}
+				>
+					&#9776;
+				</div>
+				<div
+					className={`${styles.Options} ${showOptions ? styles.show : ""}`}
+				>
+					<h1>
+						<Link to="/">Home</Link>
+					</h1>
+					<h1>
+						<Link to="/events">Event</Link>
+					</h1>
+					<h1>
+						<Link to="/team">Team</Link>
+					</h1>
+					<Link to="/contact">
+						Contact
+						{/* <button className={styles.contact}></button> */}
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default TeamPageNavbar;
