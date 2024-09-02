5831319380c2cde06efca62fc04e5fa8a92d609f
AabharanGupta <aabharangupta1001@gmail.com>
2024-09-02 11:48:26 +0530

    Revert "aa"
    This reverts commit b27591427e60fc2810d5a6b94b5afe4d79b3ad20.

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -2,32 +2,30 @@ import React from "react";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 export const Navbar = () => {
-<<<<<<< HEAD
 	const [showOptions, setShowOptions] = useState(false);
 
-  const toggleMenu = () => {
-    setShowOptions(!showOptions);
-  };
+	const toggleMenu = () => {
+		setShowOptions(!showOptions);
+	};
 
-  return (
-    <div className={styles.NavPage}>
-      <div className={styles.Header}>
+	return (
+		<div className={styles.NavPage}>
+			<div className={styles.Header}>
 				<div className={styles.logo}>
 					<img
-						src="./src/assets/images/ISTE Thapar Chapter Logo blue.png"
+						src="./src/assets/images/logo.png"
 						alt="logo"
 					/>
 				</div>
-        <div className={styles.hamburger} onClick={toggleMenu}>
-          &#9776;
-        </div>
-				<div className={`${styles.Options} ${showOptions ? styles.show : ""}`}>
-=======
-	return (
-		<div className="NavPage">
-			<div className={styles.Header}>
-				<div className={styles.Options}>
->>>>>>> bfbbbfb5176755880e7cf7370969db65ab8c2f3f
+				<div
+					className={styles.hamburger}
+					onClick={toggleMenu}
+				>
+					&#9776;
+				</div>
+				<div
+					className={`${styles.Options} ${showOptions ? styles.show : ""}`}
+				>
 					<h1>
 						<Link to="/">Home</Link>
 					</h1>
@@ -37,18 +35,10 @@ export const Navbar = () => {
 					<h1>
 						<Link to="/team">Team</Link>
 					</h1>
-<<<<<<< HEAD
-				<Link to="/contact">Contact
-					{/* <button className={styles.contact}></button> */}
-				</Link>
-=======
->>>>>>> bfbbbfb5176755880e7cf7370969db65ab8c2f3f
-				</div>
-				<div className={styles.logo}>
-					<img
-						src="logopic.jpg"
-						alt=""
-					/>
+					<Link to="/contact">
+						Contact
+						{/* <button className={styles.contact}></button> */}
+					</Link>
 				</div>
 				<Link to="/contact">
 					<button className={styles.contact}>Contact Us</button>
