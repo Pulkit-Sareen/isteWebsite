2f71b32c587f3971207a614e2c6069bff767fe8b
AabharanGupta <aabharangupta1001@gmail.com>
2024-09-03 13:35:12 +0530

    white logo

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Footer.css';
 import { Link } from 'react-router-dom';
-import { finalLogo } from '../../assets/images';
+import { whiteLogo } from '../../assets/images';
 const Footer = () => {
   return (
     <>
@@ -13,7 +13,7 @@ const Footer = () => {
         <div className="footer-container">
           <div className="footer-sections">
             <div className="footer-logo">
-              <img src={finalLogo} alt="ISTE Logo" />
+              <img src={whiteLogo} alt="ISTE Logo" />
             </div>
             <center className="footer-links">
               <Link to="/">Home</Link>
